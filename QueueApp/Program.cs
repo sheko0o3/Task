ac2ddba497aa7d90d2a2803f94cb330e24854616
unknown <sherifshams1992@gmail.com>
2024-09-27 06:59:10 +0300

    Add and Update Roi Test

--- QueueApp/Program.cs
+++ QueueApp/Program.cs
@@ -9,19 +9,9 @@ class Program
 {
     static void Main(string[] args)
     {  
-        Dictionary<string, List<string>> PreDefienedQueues = new()
-        {
-            {"A", new List<string>{"A1","A2"}},
-            {"B", new List<string>{"B1"}}
-        };
-
-        List<string> roisA = new(){"A1","A2","A3"};
-        List<string> roiB = new(){"B1","B2"};
 
         UseCase caseOne = new UseCase(usecaseID:"1");
         UseCase caseTwo = new UseCase(usecaseID:"2");
-
-
 
         Queue queueOne = new Queue(capacity:20,usecaseID:"1",queueID:"A");
         Queue queueTwo = new Queue(capacity:25, usecaseID:"1", queueID:"B");
